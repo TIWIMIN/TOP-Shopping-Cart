89d979c497f9a63454ce27a72e55d6251abc5c8f
TIWIMIN <tiwimin@gmail.com>
2025-03-12 23:15:43 -0700

    feat: Establish connection with fake store api Enables site to use information regarding store inventory

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,31 @@
+import { useState, useEffect } from "react";
+
 export default function Shop() {
+    const [items, setItems] = useState([]); 
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const getItems = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products", {
+          mode: "cors",
+          signal,
+        });
+        const data = await response.json();
+        console.log(data);
+        setItems(data); 
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getItems();
+    return () => {
+        controller.abort(); 
+    }
+  }, []);
+
   return (
     <>
       <p>Shop</p>
